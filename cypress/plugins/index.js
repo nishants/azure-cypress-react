eaea1029aa8b07b9d26e21ed21c9cb4e492b38e0
nishant <nishant.singh87@gmail.com>
2019-12-13 16:41:15 +0530

    Create perflog module.

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,46 +11,11 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-const performanceLogs = {
-  commands: [],
-  tests: []
-};
-
-const performLogsFilePath = './performanceLogs.json';
-
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
-
-const adapter = new FileSync(performLogsFilePath);
-const db = low(adapter);
-// Set some defaults (required if your JSON file is empty)
-db.defaults({
-  commands: [],
-  tests: []
-}).write();
+const { load, getTasksHandlers } = require('perflog');
 
 module.exports = (on /* ,config */) => {
+  load();
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-  on('task', {
-    performanceLog(log) {
-      // eslint-disable-next-line no-console
-      console.log(JSON.stringify(log));
-      // Add a post
-      db.get('commands')
-        .push(log)
-        .write();
-
-      return null;
-    },
-    setTestRunTime(info) {
-      performanceLogs.tests.push(info);
-      // eslint-disable-next-line no-console
-      console.log('test finished', JSON.stringify(info));
-      db.get('commands')
-        .push(info)
-        .write();
-      return null;
-    }
-  });
+  on('task', { ...getTasksHandlers() });
 };
---
+++ perflog/index.js
@@ -0,0 +1,51 @@
+/* eslint-disable no-console */
+
+const performanceLogs = {
+  commands: [],
+  tests: []
+};
+
+const performLogsFilePath = './performanceLogs.json';
+
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+const adapter = new FileSync(performLogsFilePath);
+const db = low(adapter);
+// Set some defaults (required if your JSON file is empty)
+db.defaults({
+  commands: [],
+  tests: []
+}).write();
+
+const taskHandlers = {
+  performanceLog(log) {
+    // eslint-disable-next-line no-console
+    console.log(JSON.stringify(log));
+    // Add a post
+    db.get('commands')
+      .push(log)
+      .write();
+
+    return null;
+  },
+  setTestRunTime(info) {
+    performanceLogs.tests.push(info);
+    // eslint-disable-next-line no-console
+    console.log('test finished', JSON.stringify(info));
+    db.get('commands')
+      .push(info)
+      .write();
+    return null;
+  }
+};
+
+module.exports = {
+  load: () => {
+    console.log('******************************');
+    console.log('loading buddy..be happy');
+    console.log('******************************');
+  },
+
+  getTasksHandlers: () => taskHandlers
+};
